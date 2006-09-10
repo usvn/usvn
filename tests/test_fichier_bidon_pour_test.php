bcc47591cf29568b758215ca867f39b1d4dd3c93
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2006-09-10 18:05:02 +0000

    Fichier inutile

--- tests/test_fichier_bidon_pour_test.php
+++ tests/test_fichier_bidon_pour_test.php
@@ -1,5 +1,9 @@
 <?php
 require_once 'PHPUnit/Framework/TestCase.php';
+
+$path = '/usr/lib/pear';
+set_include_path('../' . PATH_SEPARATOR . $path);
+
 require_once 'fichier_bidon_pour_test.php';
 
 class TestBidon extends PHPUnit_Framework_TestCase

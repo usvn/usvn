283c16a20af2ae03b20aa34f72ffa13c27d54d7c
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2006-11-06 18:07:54 +0000

    Le lint est lancer par cron

--- scripts/server-test.sh
+++ scripts/server-test.sh
@@ -2,6 +2,12 @@
 
 cd ~/usvn
 svn update
+phing lint > /tmp/lint.txt
+if [ $? = 1 ]
+then
+mail -s 'Echec de lint' pfe-subversion@googlegroups.com < /tmp/lint.txt
+fi
+
 phing test
 if [ $? = 1 ]
 then

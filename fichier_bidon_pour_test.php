f6cc4c1363d49c6fb875ebf1cd3f0aa1a52c4387
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2006-11-06 18:03:04 +0000

    Amelioration de la demo de phpdoc Ajout d'une regle lint permettant de verifier la syntaxe des fichiers php

--- fichier_bidon_pour_test.php
+++ fichier_bidon_pour_test.php
@@ -1,12 +1,36 @@
 <?php
+/**
+* Fichier bidon pour tester les test unitaire
+*
+* @author Julien Duponchelle
+* @package test
+* @since 0.1
+*/
+
+/**
+* Une class qui sert a rien
+*/
 class Bidon
 {
-  /**
-  * Retourne vrais
-  */
-  public function returnVrais()
-  {
-   return true;
-  }
+	/**
+	* Une fonction de demo qui retourne vrais
+	*
+	* @return bool
+	*/
+	public function returnVrais()
+	{
+		return true;
+	}
+	
+	/**
+	* Une fonction de demo qui prend une chaine
+	*
+	* @param str Chaîne à afficher
+	* @return bool
+	*/
+	public function returnVraisAvecChaine($str)
+	{
+		return true;
+	}
 }
 ?>

99b4cf3d23cfb7a8fbc9ed33b918fe808383ff8e
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2006-11-19 14:58:23 +0000

    Abbandon de simplexml pour libxml a cause de la gestion des namespaces.

--- USVN/DAV/Server/AbstractPropfindRequestHandler.php
+++ USVN/DAV/Server/AbstractPropfindRequestHandler.php
@@ -14,14 +14,21 @@ require_once 'USVN/DAV/Server/AbstractRequestHandler.php';
 
 abstract class AbstractPropfindRequestHandler extends AbstractRequestHandler
 {
+    /**
+    * Deep into xml document
+    *
+    * @var int
+    */
+    private $xml_deep = 0;
+
     function __construct($server)
     {
         parent::__construct($server);
 
-        @$xml = new SimpleXMLElement($server->getContent());
-        foreach ($xml->children('DAV:')->children('http://www.foo.bar/boxschema/') as $prop) {
-            $this->getProperty($prop->getName());
-        }
+        $xml = xml_parser_create_ns("UTF-8", " ");
+        xml_set_element_handler($xml, array(&$this, "_startTag"), array(&$this, "_endTag"));
+        xml_parser_set_option($xml, XML_OPTION_CASE_FOLDING, false); // otherwise xml_parser return tag name in upper case
+        xml_parse($xml, $server->getContent(), false);
     }
 
     /**
@@ -31,5 +38,38 @@ abstract class AbstractPropfindRequestHandler extends AbstractRequestHandler
     */
     protected function getProperty($name)
     {
+    }
+
+    /**
+     * Start tag XML handler
+     *
+     * @param  resource  XML parser
+     * @param  string    tag name
+     * @param  array     tag attributes
+     */
+    private function _startTag($parser, $name, $attrs)
+    {
+        if (strstr($name, " ")) {
+            list($namespace, $tag) = explode(" ", $name);
+        } else {
+            $namespace  = "";
+            $tag = $name;
+        }
+        if ($this->xml_deep == 2) {
+            $this->getProperty($tag);
+        }
+        $this->xml_deep++;
+    }
+
+    /**
+     * End tag XML handler
+     *
+     * @param  resource  XML parser
+     * @param  string    tag name
+     * @param  array     tag attributes
+     */
+    private function _endTag($parser, $name)
+    {
+        $this->xml_deep--;
     }
 }

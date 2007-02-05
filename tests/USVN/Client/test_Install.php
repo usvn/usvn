0a245b8e6ad90e4ca5328017d8f03ae0749f5af9
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-02-05 00:47:42 +0000

    Fichier oublier

---
+++ tests/USVN/Client/test_Install.php
@@ -0,0 +1,51 @@
+<?php
+/**
+* @package client
+* @subpackage install
+*
+* This test use UNIX commands rm and svnadmin. It's probably easy to remove
+* use of rm but you can't remove use of svnadmin.
+*/
+
+require_once 'USVN/Client/Install.php';
+
+class TestClientInstall extends PHPUnit2_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->clean();
+        exec("svnadmin create tests/tmp/testrepository");
+        mkdir('tests/tmp/fakerepository');
+    }
+
+    public function tearDown()
+    {
+        $this->clean();
+    }
+
+    private function clean()
+    {
+        exec("rm -Rf tests/tmp/testrepository");
+        @rmdir('tests/tmp/fakerepository');
+    }
+
+    public function test_notSvnRepository()
+    {
+        try
+        {
+            new USVN_Client_Install('tests/tmp/fakerepository', 'http://bidon', 'user', 'pass');
+        }
+        catch (Exception $e)
+        {
+            return;
+        }
+        $this->fail();
+    }
+
+    public function test_installHook()
+    {
+        $install = new USVN_Client_Install('tests/tmp/testrepository', 'http://bidon', 'user', 'pass');
+        $this->assertTrue(file_exists('tests/tmp/testrepository/hooks/start-commit'));
+        $this->assertTrue(file_exists('tests/tmp/testrepository/hooks/pre-commit'));
+    }
+}

b963b5922b04563ec624cad30852b938bb32ffa7
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-02-22 04:22:10 +0000

    Wrapping de l'ecriture sur la sortie d'erreur.

---
+++ USVN/Client/ConsoleUtils.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Console utils
+*
+* @author Team USVN <contact@usvn.info>
+* @link http://www.usvn.info
+* @license http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt CeCILL V2
+* @copyright Copyright 2007, Team USVN
+* @since 0.5
+* @package client
+*
+** This software has been written in EPITECH <http://www.epitech.net>
+** EPITECH is computer science school in Paris - FRANCE -
+** under the direction of Flavien Astraud <http://www.epita.fr/~flav>.
+** and Jerome Landrieu.
+**
+** Project : USVN
+** Date    : $Date$
+*/
+
+
+/**
+* Class with usefull static method for console
+*/
+class USVN_Client_ConsoleUtils
+{
+	/**
+	* @param string Write the given string on stderror.
+	*/
+	static public function writeOnStdError($str)
+	{
+		$stderr = fopen('php://stderr', 'w');
+		fwrite($stderr, $str);
+		fclose($stderr);
+	}
+}

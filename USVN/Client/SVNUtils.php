fbec7adcf4d72d7871ea1901728f654c2f1235f1
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-02-24 14:24:26 +0000

    Decoupage de la sortie de svnlook changed

--- USVN/Client/SVNUtils.php
+++ USVN/Client/SVNUtils.php
@@ -19,6 +19,10 @@ class USVN_Client_SVNUtils
                                         'pre-lock',
                                         'start-commit');
 
+	/**
+	* @param string Path of subversion repository
+	* @return bool
+	*/
     public static function isSVNRepository($path)
     {
         if (file_exists($path."/hooks") && file_exists($path."/dav"))
@@ -27,4 +31,38 @@ class USVN_Client_SVNUtils
         }
         return false;
     }
+
+	/**
+	* @param string Output of svnlook changed
+	* @return array Exemple array(array('M', 'tutu'), array('M', 'dir/tata'))
+	*/
+	public static function changedFiles($list)
+	{
+		$res = array();
+		$list = explode(" ", $list);
+		$first = true;
+		$action = "";
+		$file = "";
+		foreach ($list as $item) {
+			if ($first && in_array($item, array('M', 'D', 'A'))) {
+				if ($action) {
+					array_push($res, array($action, $file));
+				}
+				$action = $item;
+				$file = "";
+				$first = false;
+			}
+			else{
+				if ($file) {
+					$file .= " $item";
+				}
+				else {
+					$file = $item;
+				}
+				$first = true;
+			}
+		}
+		array_push($res, array($action, $file));
+		return $res;
+	}
 }
--- tests/USVN/Client/test_SVNUtils.php
+++ tests/USVN/Client/test_SVNUtils.php
@@ -34,4 +34,12 @@ class TestClientSVNUtils extends PHPUnit2_Framework_TestCase
         $this->assertTrue(USVN_Client_SVNUtils::isSVNRepository('tests/tmp/testrepository'));
         $this->assertFalse(USVN_Client_SVNUtils::isSVNRepository('tests/tmp/fakerepository'));
     }
+
+	public function test_changedFiles()
+	{
+        $this->assertEquals(array(array('M', 'tutu')), USVN_Client_SVNUtils::changedFiles('M tutu'));
+        $this->assertEquals(array(array('M', 'tutu'), array('M', 'tata')), USVN_Client_SVNUtils::changedFiles('M tutu M tata'));
+        $this->assertEquals(array(array('M', 'tutu'), array('M', 'M')), USVN_Client_SVNUtils::changedFiles('M tutu M M'));
+        $this->assertEquals(array(array('M', 'tutu'), array('M', 'hello world')), USVN_Client_SVNUtils::changedFiles('M tutu M hello world'));
+	}
 }

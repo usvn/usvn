a89cfd61d3c3ccf14551d3624d7571079883326b
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-04 02:56:48 +0000

    L'import dans MySQL marche

--- SQL/SVNDB.sql
+++ SQL/SVNDB.sql
@@ -1,49 +1,9 @@
 /*==============================================================*/
-/* DBMS name:      MySQL 4.0                                    */
-/* Created on:     11/01/2007 17:14:40                          */
+/* Table: `GROUP`                                               */
 /*==============================================================*/
-
-
-drop index TO_MANAGE_FK on PROJECT_MANAGEMENT;
-
-drop index TO_BELONG2_FK on TO_BELONG;
-
-drop index TO_BELONG_FK on TO_BELONG;
-
-drop index TO_HAVE2_FK on TO_HAVE;
-
-drop index TO_HAVE6_FK on TO_HAVE;
-
-drop index TO_HAVE_FK on TO_HAVE;
-
-drop index TO_HAVE3_FK on TO_HAVE2;
-
-drop index TO_HAVE4_FK on TO_HAVE2;
-
-drop index TO_HAVE5_FK on TO_HAVE2;
-
-drop table if exists "GROUP";
-
-drop table if exists PROJECT;
-
-drop table if exists PROJECT_MANAGEMENT;
-
-drop table if exists RIGHTS;
-
-drop table if exists TO_BELONG;
-
-drop table if exists TO_HAVE;
-
-drop table if exists TO_HAVE2;
-
-drop table if exists USERS;
-
-/*==============================================================*/
-/* Table: "GROUP"                                               */
-/*==============================================================*/
-create table "GROUP"
+create table `GROUP`
 (
-   GROUP_ID                       numeric(8,0)                   not null,
+   GROUP_ID                       INT                   not null,
    GROUP_LABEL                    varchar(100),
    GROUP_NOM                      text,
    primary key (GROUP_ID)
@@ -55,7 +15,7 @@ type = InnoDB;
 /*==============================================================*/
 create table PROJECT
 (
-   PROJECT_ID                     numeric(8,0)                   not null,
+   PROJECT_ID                     INT                   not null,
    PROJECT_NAME                   varchar(255),
    PROJECT_DATE_START             date,
    PROJECT_DESCRIPTION            text,
@@ -71,10 +31,10 @@ create table PROJECT_MANAGEMENT
    DATE                           date,
    FILENAME                       varchar(255),
    PATHFILE                       text,
-   NUM_REV                        numeric(8,0),
+   NUM_REV                        INT,
    TYP_REV                        char(1),
-   PM_ID                          numeric(8,0)                   not null,
-   PROJECT_ID                     numeric(8,0)                   not null,
+   PM_ID                          INT                   not null,
+   PROJECT_ID                     INT                   not null,
    PROPERTY                       text,
    primary key (PM_ID)
 )
@@ -93,7 +53,7 @@ create index TO_MANAGE_FK on PROJECT_MANAGEMENT
 /*==============================================================*/
 create table RIGHTS
 (
-   RIGHT_ID                       numeric(8,0)                   not null,
+   RIGHT_ID                       INT                   not null,
    RIGHT_LABEL                    varchar(255),
    primary key (RIGHT_ID)
 )
@@ -104,8 +64,8 @@ type = InnoDB;
 /*==============================================================*/
 create table TO_BELONG
 (
-   USERS_ID                       numeric(8,0)                   not null,
-   GROUP_ID                       numeric(8,0)                   not null,
+   USERS_ID                       INT                   not null,
+   GROUP_ID                       INT                   not null,
    primary key (USERS_ID, GROUP_ID)
 )
 type = InnoDB;
@@ -131,9 +91,9 @@ create index TO_BELONG2_FK on TO_BELONG
 /*==============================================================*/
 create table TO_HAVE
 (
-   RIGHT_ID                       numeric(8,0)                   not null,
-   USERS_ID                       numeric(8,0)                   not null,
-   PROJECT_ID                     numeric(8,0)                   not null,
+   RIGHT_ID                       INT                   not null,
+   USERS_ID                       INT                   not null,
+   PROJECT_ID                     INT                   not null,
    primary key (RIGHT_ID, USERS_ID, PROJECT_ID)
 )
 type = InnoDB;
@@ -167,9 +127,9 @@ create index TO_HAVE6_FK on TO_HAVE
 /*==============================================================*/
 create table TO_HAVE2
 (
-   RIGHT_ID                       numeric(8,0)                   not null,
-   GROUP_ID                       numeric(8,0)                   not null,
-   PROJECT_ID                     numeric(8,0)                   not null,
+   RIGHT_ID                       INT                   not null,
+   GROUP_ID                       INT                   not null,
+   PROJECT_ID                     INT                   not null,
    primary key (RIGHT_ID, GROUP_ID, PROJECT_ID)
 )
 type = InnoDB;
@@ -203,7 +163,7 @@ create index TO_HAVE5_FK on TO_HAVE2
 /*==============================================================*/
 create table USERS
 (
-   USERS_ID                       numeric(8,0)                   not null,
+   USERS_ID                       INT                   not null,
    USERS_LOGIN                    varchar(255)                   not null,
    USERS_PASSWD                   text                           not null,
    USERS_NOM                      text,
@@ -220,7 +180,7 @@ alter table TO_BELONG add constraint FK_TO_BELONG foreign key (USERS_ID)
       references USERS (USERS_ID) on delete restrict on update restrict;
 
 alter table TO_BELONG add constraint FK_TO_BELONG2 foreign key (GROUP_ID)
-      references "GROUP" (GROUP_ID) on delete restrict on update restrict;
+      references `GROUP` (GROUP_ID) on delete restrict on update restrict;
 
 alter table TO_HAVE add constraint FK_TO_HAVE foreign key (RIGHT_ID)
       references RIGHTS (RIGHT_ID) on delete restrict on update restrict;
@@ -235,7 +195,7 @@ alter table TO_HAVE2 add constraint FK_TO_HAVE3 foreign key (RIGHT_ID)
       references RIGHTS (RIGHT_ID) on delete restrict on update restrict;
 
 alter table TO_HAVE2 add constraint FK_TO_HAVE4 foreign key (GROUP_ID)
-      references "GROUP" (GROUP_ID) on delete restrict on update restrict;
+      references `GROUP` (GROUP_ID) on delete restrict on update restrict;
 
 alter table TO_HAVE2 add constraint FK_TO_HAVE5 foreign key (PROJECT_ID)
       references PROJECT (PROJECT_ID) on delete restrict on update restrict;

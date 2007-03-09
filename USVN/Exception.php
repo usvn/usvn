35db4044349549cbdf5e7a145e2142211f24aefa
crivis_s <crivis_s@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-09 17:03:32 +0000

    Ajout de la classe d'exception dont tout notre code doit se servir

---
+++ USVN/Exception.php
@@ -0,0 +1,16 @@
+<?php
+
+
+/**
+ * @category   USVN
+ * @package    USVN
+ */
+class USVN_Exception extends Zend_Exception {
+	public function __construct($message)
+	{
+		$params = func_get_args();
+		$params[0] = T_($message);
+		$message = call_user_func_array("sprintf", $params);
+		parent::__construct($message);
+	}
+}

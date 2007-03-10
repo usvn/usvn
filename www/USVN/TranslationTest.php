5c9529bb1066701bc6ebe6247e1167bdf882b000
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-10 22:37:21 +0000

    Migration du test du parser

--- www/USVN/TranslationTest.php
+++ www/USVN/TranslationTest.php
@@ -7,7 +7,7 @@ if (!defined("PHPUnit_MAIN_METHOD")) {
 require_once "PHPUnit/Framework/TestCase.php";
 require_once "PHPUnit/Framework/TestSuite.php";
 
-require_once 'Translation.php';
+require_once 'www/USVN/autoload.php';
 
 /**
  * Test class for USVN_Translation.

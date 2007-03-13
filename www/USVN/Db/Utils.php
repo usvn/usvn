cf8a65789f92c30a24d0e6c5605d42a24a6bb3b4
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-13 21:37:27 +0000

    Classe utilitaire pour charger un fichier SQL et effacer toute les tables de la DB.

---
+++ www/USVN/Db/Utils.php
@@ -0,0 +1,52 @@
+<?php
+/**
+* @package db
+* @since 0.5
+*/
+
+/**
+* Provide usefull static methods to manipulate database
+*/
+class USVN_Db_Utils
+{
+    /**
+    * Load an SQL file to the database
+	*
+	*  @param Zend_Db_Adapter_Abstract Connection to Database
+	*  @param string Path to the SQL file
+    */
+    public function loadFile($db, $path)
+    {
+		$query = file_get_contents($path);
+		$query = ereg_replace("--[^\n]*\n", "", $query);
+		$tab = explode(";", $query);
+		foreach($tab as $ligne) {
+			$ligne = trim($ligne, " \n\t");
+			$ligne = rtrim($ligne, " \n\t");
+			if (strlen($ligne)) {
+				$db->query($ligne);
+			}
+		}
+	}
+
+	/**
+	* Delete all tables from the database.
+	*
+	*  Warning this code can do an infinity loop in case of problems.
+	*
+	*  @param Zend_Db_Adapter_Abstract Connection to Database
+    */
+	public function deleteAllTables($db)
+	{
+		$todelete = $db->listTables();
+		while (count($todelete)) {
+			$table = array_shift($todelete);
+			try {
+				$db->query("DROP TABLE $table CASCADE");
+			}
+			catch (Exception $e) {
+				array_push($todelete, $table);
+			}
+		}
+	}
+}

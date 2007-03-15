f97e6c3e2ae7da937624d15cb4ec1d9aac0531e5
crivis_s <crivis_s@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-15 17:50:48 +0000

    - Ajout du module admin dans lequel la plupart des controlleurs que l'on doit faire pour le moment devront aller.
    - Debut de la gestion des projets

---
+++ www/USVN/modules/admin/controllers/IndexController.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'USVN/modules/default/controllers/IndexController.php';
+
+class admin_IndexController extends IndexController
+{
+}
---
+++ www/USVN/modules/admin/controllers/ProjectController.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'USVN/modules/admin/controllers/IndexController.php';
+
+class admin_ProjectController extends admin_IndexController {
+	public function newAction()
+	{
+
+	}
+
+	public function editAction()
+	{
+
+	}
+
+	public function deleteAction()
+	{
+
+	}
+}

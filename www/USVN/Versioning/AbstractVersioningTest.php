672527bfec09ff57fea249f7215bfd0b4b95d285
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-18 02:18:32 +0000

    Tres leger rangement, en prévision de plus de test sur le versioning.

---
+++ www/USVN/Versioning/AbstractVersioningTest.php
@@ -0,0 +1,37 @@
+<?php
+require_once "PHPUnit/Framework/TestCase.php";
+require_once "PHPUnit/Framework/TestSuite.php";
+
+require_once 'www/USVN/autoload.php';
+
+class USVN_Versioning_AbstractVersioningTest extends USVN_Test_DB {
+	public function setUp()
+	{
+		parent::setUp();
+		$data = array(
+			"projects_id" => 1,
+			"projects_name" => 'Projet Love'
+		);
+		$this->db->insert("projects", $data);
+
+		$data = array(
+			"files_id" => 1,
+			"files_filename" => 'test',
+			"files_path" => 'trunk/',
+			"files_num_rev" => 1,
+			"projects_id" => 1
+		);
+		$this->db->insert("files", $data);
+
+		$data = array(
+			"files_id" => 2,
+			"files_filename" => 'test',
+			"files_path" => 'trunk/',
+			"files_num_rev" => 2,
+			"projects_id" => 1
+		);
+		$this->db->insert("files", $data);
+	}
+
+}
+?>

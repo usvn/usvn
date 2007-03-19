8604996a5b0b99e8fa037c85aed10b0bfe2880f9
crivis_s <crivis_s@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-19 10:48:56 +0000

    - modification de l'heritage d'admin_UserController pour etre sur admin_IndexController - suppression des doublons de code (indexAction() et _render() qui sont heritees et non modifiees...)

--- www/USVN/modules/admin/controllers/UserController.php
+++ www/USVN/modules/admin/controllers/UserController.php
@@ -2,34 +2,8 @@
 
 require_once 'USVN/modules/admin/controllers/IndexController.php';
 
-class admin_UserController extends IndexController
+class admin_UserController extends admin_IndexController
 {
-	public function indexAction()
-	{
-		$this->_render();
-	}
-
-	/**
-	 * A simple wrapper to render a template.
-	 *
-	 * It actually get the Response object, set the Content-Type to 'text/html' and
-	 * render our $template.
-	 *
-	 * If $template is null, it will automaticaly detect our current action and try to
-	 * render the $action template.
-	 *
-	 * @param string $template
-	 */
-	protected function _render($template = null)
-	{
-		if ($template === null) {
-			$template = $this->getRequest()->getActionName() . ".html";
-		}
-		$this->getResponse()
-			->setHeader('Content-Type', 'text/html')
-			->appendBody($this->_view->render($template));
-	}
-
 	public function addAction()
 	{
 		$user = new USVN_modules_admin_models_Users();

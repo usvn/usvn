96d8a4a65cbd462a238b42e03bde9c6c112522c0
guyoll_o <guyoll_o@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-19 15:40:22 +0000

    MAJ DB prefixage des tables par usvn_

--- SQL/SVNDB.sql
+++ SQL/SVNDB.sql
@@ -1,247 +1,153 @@
 /*==============================================================*/
-/* Table: files                                                 */
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     19/03/2007 16:38:03                          */
 /*==============================================================*/
-create table files
+
+
+/*==============================================================*/
+/* Table: usvn_files                                            */
+/*==============================================================*/
+create table usvn_files
 (
-   files_id                       int                            not null,
-   projects_id                    int                            not null,
-   files_filename                 varchar(255)                   not null,
-   files_path                     text                           not null,
-   files_date                     datetime,
-   files_isdir                    bool,
-   files_num_rev                  int,
-   files_typ_rev                  char(1),
-   files_message                  varchar(1000),
+   files_id             int not null,
+   projects_id          int not null,
+   files_filename       varchar(255) not null,
+   files_path           text not null,
+   files_date           datetime,
+   files_isdir          bool,
+   files_num_rev        int,
+   files_typ_rev        char(1),
+   files_message        varchar(1000),
    primary key (files_id)
-)
-type = InnoDB;
-
-
-/*==============================================================*/
-/* Index: to_manage_fk                                          */
-/*==============================================================*/
-create index to_manage_fk on files
-(
-   projects_id
 );
 
 /*==============================================================*/
-/* Table: groups                                                */
+/* Table: usvn_groups                                           */
 /*==============================================================*/
-create table groups
+create table usvn_groups
 (
-   groups_id                      int                            not null,
-   groups_name                    varchar(150)                   not null,
+   groups_id            int not null,
+   groups_name          varchar(150) not null,
    primary key (groups_id)
-)
-type = InnoDB;
-
+);
 
 /*==============================================================*/
-/* Table: projects                                              */
+/* Table: usvn_projects                                         */
 /*==============================================================*/
-create table projects
+create table usvn_projects
 (
-   projects_id                    int                            not null,
-   projects_name                  varchar(255)                   not null,
-   projects_date_start            datetime,
-   projects_description           varchar(1000),
+   projects_id          int not null,
+   projects_name        varchar(255) not null,
+   projects_date_start  datetime,
+   projects_description varchar(1000),
    primary key (projects_id)
-)
-type = InnoDB;
-
+);
 
 /*==============================================================*/
-/* Table: properties                                            */
+/* Table: usvn_properties                                       */
 /*==============================================================*/
-create table properties
+create table usvn_properties
 (
-   properties_id                  int                            not null,
-   properties_version             int                            not null,
-   properties_value               text                           not null,
-   properties_label_property      varchar(64)                    not null,
-   properties_path                varchar(255)                   not null,
+   properties_id        int not null,
+   properties_version   int not null,
+   properties_value     text not null,
+   properties_label_property varchar(64) not null,
+   properties_path      varchar(255) not null,
    primary key (properties_id)
-)
-type = InnoDB;
-
+);
 
 /*==============================================================*/
-/* Table: rights                                                */
+/* Table: usvn_rights                                           */
 /*==============================================================*/
-create table rights
+create table usvn_rights
 (
-   rights_id                      int                            not null,
-   rights_label                   varchar(255)                   not null,
+   rights_id            int not null,
+   rights_label         varchar(255) not null,
    primary key (rights_id)
-)
-type = InnoDB;
-
+);
 
 /*==============================================================*/
-/* Table: to_assign                                             */
+/* Table: usvn_to_assign                                        */
 /*==============================================================*/
-create table to_assign
+create table usvn_to_assign
 (
-   properties_id                  int                            not null,
-   files_id                       int                            not null,
+   properties_id        int not null,
+   files_id             int not null,
    primary key (properties_id, files_id)
-)
-type = InnoDB;
-
-
-/*==============================================================*/
-/* Index: to_assign_fk                                          */
-/*==============================================================*/
-create index to_assign_fk on to_assign
-(
-   properties_id
 );
 
 /*==============================================================*/
-/* Index: to_assign2_fk                                         */
+/* Table: usvn_to_attribute                                     */
 /*==============================================================*/
-create index to_assign2_fk on to_assign
+create table usvn_to_attribute
 (
-   files_id
-);
-
-/*==============================================================*/
-/* Table: to_attribute                                          */
-/*==============================================================*/
-create table to_attribute
-(
-   rights_id                      int                            not null,
-   groups_id                      int                            not null,
-   projects_id                    int                            not null,
+   rights_id            int not null,
+   groups_id            int not null,
+   projects_id          int not null,
    primary key (rights_id, groups_id, projects_id)
-)
-type = InnoDB;
-
-
-/*==============================================================*/
-/* Index: to_attribute_fk                                       */
-/*==============================================================*/
-create index to_attribute_fk on to_attribute
-(
-   rights_id
 );
 
 /*==============================================================*/
-/* Index: to_attribute2_fk                                      */
+/* Table: usvn_to_belong                                        */
 /*==============================================================*/
-create index to_attribute2_fk on to_attribute
+create table usvn_to_belong
 (
-   groups_id
-);
-
-/*==============================================================*/
-/* Index: to_attribute3_fk                                      */
-/*==============================================================*/
-create index to_attribute3_fk on to_attribute
-(
-   projects_id
-);
-
-/*==============================================================*/
-/* Table: to_belong                                             */
-/*==============================================================*/
-create table to_belong
-(
-   users_id                       int                            not null,
-   groups_id                      int                            not null,
+   users_id             int not null,
+   groups_id            int not null,
    primary key (users_id, groups_id)
-)
-type = InnoDB;
-
-
-/*==============================================================*/
-/* Index: to_belong_fk                                          */
-/*==============================================================*/
-create index to_belong_fk on to_belong
-(
-   users_id
 );
 
 /*==============================================================*/
-/* Index: to_belong2_fk                                         */
+/* Table: usvn_to_have                                          */
 /*==============================================================*/
-create index to_belong2_fk on to_belong
+create table usvn_to_have
 (
-   groups_id
-);
-
-/*==============================================================*/
-/* Table: to_have                                               */
-/*==============================================================*/
-create table to_have
-(
-   users_id                       int                            not null,
-   projects_id                    int                            not null,
+   users_id             int not null,
+   projects_id          int not null,
    primary key (users_id, projects_id)
-)
-type = InnoDB;
-
-
-/*==============================================================*/
-/* Index: to_have_fk                                            */
-/*==============================================================*/
-create index to_have_fk on to_have
-(
-   users_id
 );
 
 /*==============================================================*/
-/* Index: to_have2_fk                                           */
+/* Table: usvn_users                                            */
 /*==============================================================*/
-create index to_have2_fk on to_have
+create table usvn_users
 (
-   projects_id
-);
-
-/*==============================================================*/
-/* Table: users                                                 */
-/*==============================================================*/
-create table users
-(
-   users_id                       int                            not null,
-   users_login                    varchar(255)                   not null,
-   users_password                 varchar(44)                    not null,
-   users_lastname                 varchar(100),
-   users_firstname                varchar(100),
-   users_email                    varchar(150),
+   users_id             int not null,
+   users_login          varchar(255) not null,
+   users_password       varchar(44) not null,
+   users_lastname       varchar(100),
+   users_firstname      varchar(100),
+   users_email          varchar(150),
    primary key (users_id)
-)
-type = InnoDB;
+);
 
+alter table usvn_files add constraint fk_usvn_to_manage foreign key (projects_id)
+      references usvn_projects (projects_id) on delete restrict on update restrict;
 
-alter table files add constraint fk_to_manage foreign key (projects_id)
-      references projects (projects_id) on delete restrict on update restrict;
+alter table usvn_to_assign add constraint fk_usvn_to_assign foreign key (properties_id)
+      references usvn_properties (properties_id) on delete restrict on update restrict;
 
-alter table to_assign add constraint fk_to_assign foreign key (properties_id)
-      references properties (properties_id) on delete restrict on update restrict;
+alter table usvn_to_assign add constraint fk_usvn_to_assign2 foreign key (files_id)
+      references usvn_files (files_id) on delete restrict on update restrict;
 
-alter table to_assign add constraint fk_to_assign2 foreign key (files_id)
-      references files (files_id) on delete restrict on update restrict;
+alter table usvn_to_attribute add constraint fk_usvn_to_attribute foreign key (rights_id)
+      references usvn_rights (rights_id) on delete restrict on update restrict;
 
-alter table to_attribute add constraint fk_to_attribute foreign key (rights_id)
-      references rights (rights_id) on delete restrict on update restrict;
+alter table usvn_to_attribute add constraint fk_usvn_to_attribute2 foreign key (groups_id)
+      references usvn_groups (groups_id) on delete restrict on update restrict;
 
-alter table to_attribute add constraint fk_to_attribute2 foreign key (groups_id)
-      references groups (groups_id) on delete restrict on update restrict;
+alter table usvn_to_attribute add constraint fk_usvn_to_attribute3 foreign key (projects_id)
+      references usvn_projects (projects_id) on delete restrict on update restrict;
 
-alter table to_attribute add constraint fk_to_attribute3 foreign key (projects_id)
-      references projects (projects_id) on delete restrict on update restrict;
+alter table usvn_to_belong add constraint fk_usvn_to_belong foreign key (users_id)
+      references usvn_users (users_id) on delete restrict on update restrict;
 
-alter table to_belong add constraint fk_to_belong foreign key (users_id)
-      references users (users_id) on delete restrict on update restrict;
+alter table usvn_to_belong add constraint fk_usvn_to_belong2 foreign key (groups_id)
+      references usvn_groups (groups_id) on delete restrict on update restrict;
 
-alter table to_belong add constraint fk_to_belong2 foreign key (groups_id)
-      references groups (groups_id) on delete restrict on update restrict;
+alter table usvn_to_have add constraint fk_usvn_to_have foreign key (users_id)
+      references usvn_users (users_id) on delete restrict on update restrict;
 
-alter table to_have add constraint fk_to_have foreign key (users_id)
-      references users (users_id) on delete restrict on update restrict;
-
-alter table to_have add constraint fk_to_have2 foreign key (projects_id)
-      references projects (projects_id) on delete restrict on update restrict;
+alter table usvn_to_have add constraint fk_usvn_to_have2 foreign key (projects_id)
+      references usvn_projects (projects_id) on delete restrict on update restrict;
 

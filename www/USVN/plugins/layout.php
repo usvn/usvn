fab0107f999acb295ca1e7a72c80d9993af63635
crivis_s <crivis_s@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-19 10:45:48 +0000

    Ajout du code pour le layout.
    Il ne reste plus qu'a faire une vraie generation de menu/header/footer...

--- www/USVN/plugins/layout.php
+++ www/USVN/plugins/layout.php
@@ -1,12 +1,65 @@
 <?php
 
-class layout extends Zend_Controller_Plugin_Abstract 
+class layout extends Zend_Controller_Plugin_Abstract
 {
 	/**
 	 * Doit gerer le layout de la page HTML
 	 *
 	 */
-	public function dispatchLoopEnd()
+	public function dispatchLoopShutdown()
 	{
+		$response = $this->getResponse();
+		$headers = $response->getHeaders();
+		foreach ($headers as $header) {
+			if (strcasecmp($header['name'], "Content-Type") == 0) {
+				if (strcasecmp($header['value'], "text/html") == 0) {
+					$this->addHeader($response);
+					$this->addFooter($response);
+				}
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Add header text
+	 *
+	 * @param Zend_Controller_Response_Abstract $response
+	 */
+	protected function addHeader($response)
+	{
+		$header = <<<EOF
+<html>
+	<head>
+	</head>
+	<body>
+		<div id="usvn_header"></div>
+		<div id="usvn_menu"></div>
+		<div id="usvn_content">
+
+EOF;
+		$body = $response->getBody(true);
+		$response->setBody($header);
+		foreach ($body as $text) {
+			$response->appendBody($text);
+		}
+	}
+
+	/**
+	 * Add footer text
+	 *
+	 * @param Zend_Controller_Response_Abstract $response
+	 */
+	protected function addFooter($response)
+	{
+		$footer = <<<EOF
+
+		</div>
+		<div id="usvn_footer">Powered by USVN</div>
+	</body>
+</html>
+
+EOF;
+		$response->appendBody($footer);
 	}
 }

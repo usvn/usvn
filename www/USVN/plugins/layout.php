1ba00578c631afd35d17dde555de0155779bcc3e
dolean_j <dolean_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-20 15:35:33 +0000

    correctif pour la prise en charge de BASE_URL.
    Attention: n'est pas gere pour les fichiers statics se trouvant dans medias comme les fichiers css, javascript incorporant des paths
    Exemple: image de background.
    
    Notice: Devra etre gere par l'installateur en faisant de la reecriture de path (specification d'un tag a remplacer dans le code ou autre).

--- www/USVN/plugins/layout.php
+++ www/USVN/plugins/layout.php
@@ -28,27 +28,28 @@ class layout extends Zend_Controller_Plugin_Abstract
 	 */
 	protected function addHeader($response)
 	{
+		$base_url = BASE_URL;
 		$header = <<<EOF
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr" xml:lang="fr">
-<head>
-    <title>USVN</title>
-    <link type="text/css" rel="stylesheet" media="screen" href="/medias/css/screen.css" />
-	<link type="text/css" rel="stylesheet" media="print" href="/medias/css/print.css"  />
-	<link rel="icon" href="/medias/images/USVN.ico" type="image/x-icon" />
-    <meta http-equiv="Content-Type"	content="text/html; charset=UTF-8" />
-    <script language="JavaScript" type="text/javascript" src="/medias/js/usvn.js"></script>
-</head>
-<body>
-	<div id="usvn_banner">
-		<div id="usvn_header">
-			<a id="usvn_logo" href="/">
-				<img src="/medias/images/USVN-logo.png" alt="USVN, Userfriendly SVN" />
-			</a>
+	<head>
+	    <title>USVN</title>
+	    <link type="text/css" rel="stylesheet" media="screen" href="{$base_url}/medias/css/screen.css" />
+		<link type="text/css" rel="stylesheet" media="print" href="{$base_url}/medias/css/print.css"  />
+		<link rel="icon" href="{$base_url}/medias/images/USVN.ico" type="image/x-icon" />
+	    <meta http-equiv="Content-Type"	content="text/html; charset=UTF-8" />
+	    <script language="JavaScript" type="text/javascript" src="{$base_url}/medias/js/usvn.js"></script>
+	</head>
+	<body>
+		<div id="usvn_banner">
+			<div id="usvn_header">
+				<a id="usvn_logo" href="/">
+					<img src="{$base_url}/medias/images/USVN-logo.png" alt="USVN, Userfriendly SVN" />
+				</a>
+			</div>
 		</div>
-	</div>
-	<div id="usvn_menu"></div>
-	<div id="usvn_content">
+		<div id="usvn_menu"></div>
+		<div id="usvn_content">
 EOF;
 		$body = $response->getBody(true);
 		$response->setBody($header);
@@ -65,12 +66,10 @@ EOF;
 	protected function addFooter($response)
 	{
 		$footer = <<<EOF
-
 		</div>
 		<div id="usvn_footer">Powered by USVN</div>
 	</body>
 </html>
-
 EOF;
 		$response->appendBody($footer);
 	}

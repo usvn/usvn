5f5c213a87658b1da926845f84198a697bc3352c
crivis_s <crivis_s@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-21 14:36:12 +0000

    Ajout du debut de la gestion des projets
    reference : #46

--- www/USVN/modules/admin/controllers/ProjectController.php
+++ www/USVN/modules/admin/controllers/ProjectController.php
@@ -3,14 +3,67 @@
 require_once 'USVN/modules/admin/controllers/IndexController.php';
 
 class admin_ProjectController extends admin_IndexController {
+	public function indexAction()
+	{
+		$table = new USVN_modules_default_models_Projects();
+		$this->_view->projects = $table->fetchAll(null, "projects_name");
+		$this->_render();
+	}
+
 	public function newAction()
 	{
-
+		if (!empty($_POST)) {
+			$this->save("USVN_modules_default_models_Projects", "project");
+		}
+		$this->_render('form.html');
 	}
 
 	public function editAction()
 	{
+		if (!empty($_POST)) {
+			$this->save("USVN_modules_default_models_Projects", "project");
+		}
+		$this->_render('form.html');
+	}
 
+	protected function save($className, $name)
+	{
+		$table = new $className();
+		/* @var $table USVN_Db_Table */
+
+		$info = $table->info();
+
+		$primary = array_intersect_key($_POST, array_keys($info['primary']));
+
+		if ($primary) {
+			$obj = $table->find($primary);
+		} else {
+			$obj = $table->fetchNew();
+		}
+		unset($_POST[$primaryKey]);
+		$obj->setFromArray($_POST);
+
+		try {
+			$obj->save();
+			if ($this->getRequest()->getActionName() == 'new') {
+				$this->saveAfterNew($obj);
+			}
+		} catch (Exception $e) {
+			$this->_view->exception = $e;
+			$this->_render('exception.html');
+			$this->_view->clearVars();
+		}
+		$this->_view->$name = $obj;
+	}
+
+	/**
+	 *
+	 *
+	 * @param USVN_Db_Table_Row $obj
+	 */
+	protected function saveAfterNew($obj)
+	{
+		$this->_redirect("/admin/project/edit/{$obj->name}/");
 	}
 
 	public function deleteAction()
---
+++ www/USVN/modules/default/models/Projects.php
@@ -0,0 +1,27 @@
+<?php
+
+class USVN_modules_default_models_Projects extends USVN_Db_Table {
+	/**
+	 * The primary key column (underscore format).
+	 *
+	 * Without our prefixe...
+	 *
+	 * @var string
+	 */
+	protected $_primary = "id";
+
+	/**
+	 * The field's prefix for this table.
+	 *
+	 * @var string
+	 */
+	protected $_fieldPrefix = "projects_";
+
+	/**
+	 * The table name derived from the class name (underscore format).
+	 *
+	 * @var array
+	 */
+    protected $_name = "projects";
+
+}

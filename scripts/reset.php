198ec9dee870a435045becc5e6ee89782e19e41b
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-03-26 22:19:13 +0000

    phing reset permet de reinstaller la DB
    closes #120

---
+++ scripts/reset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Reinstall database
+ *
+ * @author Team USVN <contact@usvn.info>
+ * @link http://www.usvn.info
+ * @license http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt CeCILL V2
+ * @copyright Copyright 2007, Team USVN
+ * @since 0.5
+ * @package utils
+ *
+ * This software has been written at EPITECH <http://www.epitech.net>
+ * EPITECH, European Institute of Technology, Paris - FRANCE -
+ * This project has been realised as part of
+ * end of studies project.
+ *
+ * $Id$
+ */
+
+require_once('www/USVN/autoload.php');
+
+$config = new USVN_Config("www/USVN/config.ini", "general");
+
+$db = Zend_Db::factory($config->database->adapterName, $config->database->options->asArray());
+Zend_Db_Table::setDefaultAdapter($db);
+USVN_Db_Utils::deleteAllTables($db);
+USVN_Db_Utils::loadFile($db, "SQL/SVNDB.sql");

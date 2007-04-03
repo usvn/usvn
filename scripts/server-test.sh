f5ec0c951862286a05c3e464a8e132306f5c0a7a
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-04-03 20:04:19 +0000

    Meme si le rapport est vide il faut tout de meme envoyer le mail
     refs #171

--- scripts/server-test.sh
+++ scripts/server-test.sh
@@ -6,6 +6,6 @@ svn update
 phing test
 if [ $? != 0 ]
 then
-`echo "http://testunit.usvn.info/\n\n" && cat test-report/logfile.txt` | mail -s 'Echec des tests unitaire' pfe-subversion@googlegroups.com
+echo "http://testunit.usvn.info/\n\n" | mail -s 'Echec des tests unitaire' pfe-subversion@googlegroups.com
 fi
 phing cover

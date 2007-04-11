33c8ae90092e8a31294fcedd4fe4327cf55dce61
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-04-11 13:16:19 +0000

    On exclue les controlleurs de la couverture de code.
    closes #187

--- www/USVN/Db/VersionnableTable.php
+++ www/USVN/Db/VersionnableTable.php
@@ -7,7 +7,7 @@
  * @license http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt CeCILL V2
  * @copyright Copyright 2007, Team USVN
  * @since 0.5
- * @package db
+ * @package USVN_Db
  * @subpackage versionnable
  *
  * This software has been written at EPITECH <http://www.epitech.net>

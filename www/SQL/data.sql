3589555ea125a6a96a95361e9d2d6d4ed0c86a12
attal_m <attal_m@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-04-25 17:24:25 +0000

    Pb avec le fichier data.sql
    refs #57

--- www/SQL/data.sql
+++ www/SQL/data.sql
@@ -1,21 +1,13 @@
-
+INSERT INTO `usvn_users` (`users_id`, `users_login`, `users_password`, `users_lastname`, `users_firstname`, `users_email`) VALUES 
+(1, 'anonymous', 'usvn', 'anonymous', 'anonymous', 'anonymous@anonymous.com'),
+(2, 'user', '$1$XE0.Uo1.$g3tY8fdDeZa/K6qeHIHLi0', 'user', NULL, NULL),
+(3, 'admin', '$1$TF..gu5.$ZOLlWKWvnphIws40vZqjH0', 'admin', 'admin', 'admin@admin.com');
 
 INSERT INTO `usvn_groups` (`groups_id`, `groups_name`, `groups_description`) VALUES 
 (1, 'Anonymous', NULL),
 (2, 'Users', NULL),
 (3, 'Admins', NULL);
 
-
-INSERT INTO `usvn_modules` (`modules_id`, `modules_name`, `modules_description`) VALUES 
-(1, 'default', 'default  module'),
-(2, 'admin', 'admin module'),
-(3, 'wiki', 'wiki module');
-
-
-INSERT INTO `usvn_modules_to_projects` (`modules_id`, `projects_id`) VALUES 
-(1, 1),
-(2, 1),
-(3, 1);
 
 
 INSERT INTO `usvn_projects` (`projects_id`, `projects_name`, `projects_start_date`, `projects_description`, `projects_auth`, `projects_url`) VALUES 
@@ -48,11 +40,6 @@ INSERT INTO `usvn_rights` (`rights_id`, `rights_label`, `rights_description`) VA
 (22, 'wiki_index', NULL);
 
 
-INSERT INTO `usvn_users` (`users_id`, `users_login`, `users_password`, `users_lastname`, `users_firstname`, `users_email`) VALUES 
-(1, 'anonymous', 'usvn', 'anonymous', 'anonymous', 'anonymous@anonymous.com'),
-(2, 'user', '$1$XE0.Uo1.$g3tY8fdDeZa/K6qeHIHLi0', 'user', NULL, NULL),
-(3, 'admin', '$1$TF..gu5.$ZOLlWKWvnphIws40vZqjH0', 'admin', 'admin', 'admin@admin.com');
-
 
 INSERT INTO `usvn_users_to_groups` (`users_id`, `groups_id`) VALUES 
 (1, 1),
@@ -76,3 +63,14 @@ INSERT INTO `usvn_workgroups_to_rights` (`workgroups_id`, `rights_id`, `is_right
 (7, 12, 1),
 (7, 16, 1),
 (7, 20, 1);
+
+INSERT INTO `usvn_modules` (`modules_id`, `modules_name`, `modules_description`) VALUES 
+(1, 'default', 'default  module'),
+(2, 'admin', 'admin module'),
+(3, 'wiki', 'wiki module');
+
+
+INSERT INTO `usvn_modules_to_projects` (`modules_id`, `projects_id`) VALUES 
+(1, 1),
+(2, 1),
+(3, 1);

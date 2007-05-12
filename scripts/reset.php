b8e35174aa5163718228a4a2d8c12ac67688777b
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-05-12 15:24:29 +0000

    Le script reset n'utiliser pas les modeles donc pas de generation  du htpasswd
    refs #235

--- scripts/reset.php
+++ scripts/reset.php
@@ -20,12 +20,91 @@
 require_once('www/USVN/autoload.php');
 
 $config = new USVN_Config("www/config.ini", "general");
+Zend_Registry::set('config', $config);
+USVN_Translation::initTranslation('en_US', 'www/locale');
 
 $db = Zend_Db::factory($config->database->adapterName, $config->database->options->asArray());
 USVN_Db_Table::$prefix = $config->database->prefix;
 Zend_Db_Table::setDefaultAdapter($db);
 USVN_Db_Utils::deleteAllTables($db);
+USVN_DirectoryUtils::removeDirectory("www/files");
+mkdir("www/files");
 USVN_Db_Utils::loadFile($db, "www/SQL/SVNDB.sql");
 USVN_Db_Utils::loadFile($db, "www/SQL/mysql.sql");
 USVN_Db_Utils::loadFile($db, "www/SQL/data.sql");
-USVN_Db_Utils::loadFile($db, "www/SQL/test.sql");
+
+$table = new USVN_Db_Table_Users();
+$obj = $table->fetchNew();
+$obj->setFromArray(array('users_login' 		=> 'admin',
+						'users_password' 	=> 'adpexzg3FUZAk',
+						'users_firstname' 	=> 'System',
+						'users_lastname' 	=> 'Administrator',
+						'users_email' 		=> ''));
+$obj->save();
+
+$obj = $table->fetchNew();
+$obj->setFromArray(array('users_login' 		=> 'attal_m',
+						'users_password' 	=> 'atxBfaSI43jDs',
+						'users_firstname' 	=> 'Mathieu',
+						'users_lastname' 	=> 'Attal',
+						'users_email' 		=> ''));
+$obj->save();
+
+$obj = $table->fetchNew();
+$obj->setFromArray(array('users_login' 		=> 'billar_m',
+						'users_password' 	=> 'bicTTCzMDeU4',
+						'users_firstname' 	=> 'Marie',
+						'users_lastname' 	=> 'Billard',
+						'users_email' 		=> ''));
+$obj->save();
+
+$obj = $table->fetchNew();
+$obj->setFromArray(array('users_login' 		=> 'crivis_s',
+						'users_password' 	=> 'crGzUc/D4cb5U',
+						'users_firstname' 	=> 'Stephane',
+						'users_lastname' 	=> 'Crivisier',
+						'users_email' 		=> ''));
+$obj->save();
+
+$obj = $table->fetchNew();
+$obj->setFromArray(array('users_login' 		=> 'dolean_j',
+						'users_password' 	=> '$1$PRxOzeKA$Ij5uFSIk41M9B2Lk0d8Ml1',
+						'users_firstname' 	=> 'Julien',
+						'users_lastname' 	=> 'Doleans',
+						'users_email' 		=> ''));
+$obj->save();
+
+$obj = $table->fetchNew();
+$obj->setFromArray(array('users_login' 		=> 'duponc_j',
+						'users_password' 	=> 'duMve6sI7pq3E',
+						'users_firstname' 	=> 'Julien',
+						'users_lastname' 	=> 'Duponchelle',
+						'users_email' 		=> ''));
+$obj->save();
+
+$obj = $table->fetchNew();
+$obj->setFromArray(array('users_login' 		=> 'guyoll_o',
+						'users_password' 	=> 'gu2JqgcmE3/II',
+						'users_firstname' 	=> 'Olivier',
+						'users_lastname' 	=> 'Guyollot',
+						'users_email' 		=> ''));
+$obj->save();
+
+$obj = $table->fetchNew();
+$obj->setFromArray(array('users_login' 		=> 'joanic_g',
+						'users_password' 	=> 'jok6qdG.SljvQ',
+						'users_firstname' 	=> 'Gabriel',
+						'users_lastname' 	=> 'Joanicot',
+						'users_email' 		=> ''));
+$obj->save();
+
+
+$table = new USVN_Db_Table_Projects();
+$obj = $table->fetchNew();
+$obj->setFromArray(array('projects_name' => 'usvn',  'projects_start_date' => '2007-11-01 00:00:00'));
+$id = $obj->save();
+
+$table = new USVN_Db_Table_Projects();
+$obj = $table->fetchNew();
+$obj->setFromArray(array('projects_name' => 'love',  'projects_start_date' => '1984-12-03 00:00:00'));
+$id = $obj->save();

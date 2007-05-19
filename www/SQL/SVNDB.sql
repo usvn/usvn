ac068c889512d7bfc266e18fa5be7eef2056d9de
guyoll_o <guyoll_o@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-05-19 08:51:46 +0000

    MAJ de la database : Suppression des tables workgroups et rights
    refs #177

--- www/SQL/SVNDB.sql
+++ www/SQL/SVNDB.sql
@@ -1,4 +1,4 @@
-create table usvn_projects
+﻿create table usvn_projects
 (
    projects_id          int not null,
    projects_name        varchar(255) not null,
@@ -39,12 +39,15 @@ create table usvn_groups_to_files_rights
       references usvn_groups (groups_id) on delete restrict on update restrict
 );
 
-create table usvn_rights
+create table usvn_groups_to_projects
 (
-   rights_id            int not null,
-   rights_label         varchar(255) not null,
-   rights_description   varchar(1000),
-   primary key (rights_id)
+   projects_id          int not null,
+   groups_id            int not null,
+   primary key (projects_id, groups_id),
+   constraint fk_usvn_groups_to_projects foreign key (projects_id)
+      references usvn_projects (projects_id) on delete restrict on update restrict,
+   constraint fk_usvn_groups_to_projects2 foreign key (groups_id)
+      references usvn_groups (groups_id) on delete restrict on update restrict
 );
 
 create table usvn_users
@@ -69,26 +72,3 @@ create table usvn_users_to_groups
       references usvn_groups (groups_id) on delete restrict on update restrict
 );
 
-create table usvn_workgroups
-(
-   workgroups_id        int not null,
-   projects_id          int not null,
-   groups_id            int not null,
-   primary key (workgroups_id),
-   constraint fk_usvn_projects_to_workgroups foreign key (projects_id)
-      references usvn_projects (projects_id) on delete restrict on update restrict,
-   constraint fk_usvn_groups_to_workgroups foreign key (groups_id)
-      references usvn_groups (groups_id) on delete restrict on update restrict
-);
-
-create table usvn_workgroups_to_rights
-(
-   workgroups_id        int not null,
-   rights_id            int not null,
-   is_right             bool,
-   primary key (workgroups_id, rights_id),
-   constraint fk_usvn_workgroups_to_rights foreign key (workgroups_id)
-      references usvn_workgroups (workgroups_id) on delete restrict on update restrict,
-   constraint fk_usvn_workgroups_to_rights2 foreign key (rights_id)
-      references usvn_rights (rights_id) on delete restrict on update restrict
-);

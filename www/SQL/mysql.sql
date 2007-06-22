28c50013c866adf00d759294c7b7c0776b8280f0
guyoll_o <guyoll_o@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-06-22 17:51:14 +0000

    correction mysql.sql
    refs #457

--- www/SQL/mysql.sql
+++ www/SQL/mysql.sql
@@ -154,6 +154,3 @@ ALTER TABLE `usvn_groups` CHANGE `groups_id` `groups_id` INT( 11 ) NOT NULL AUTO
 ALTER TABLE `usvn_projects` CHANGE `projects_id` `projects_id` INT( 11 ) NOT NULL AUTO_INCREMENT ;
 ALTER TABLE `usvn_users` CHANGE `users_id` `users_id` INT( 11 ) NOT NULL AUTO_INCREMENT ;
 ALTER TABLE `usvn_files_rights` CHANGE `files_rights_id` `files_rights_id` INT( 11 ) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE usvn_projects DROP COLUMN projects_url;
-ALTER TABLE usvn_users_to_groups ADD is_leader bool;
-ALTER TABLE usvn_users ADD UNIQUE (users_login);

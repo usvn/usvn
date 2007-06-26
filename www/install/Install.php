c32b65e119e7d88ecb4b6f9dc77c0b1518c1ba73
crivis_s <crivis_s@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-06-26 07:27:26 +0000

    * Corrige le numero de version dans l'installation
    refs #467

--- www/install/Install.php
+++ www/install/Install.php
@@ -275,7 +275,7 @@ EOF;
 	static public function installEnd($config_file)
 	{
 		$config = Install::_loadConfig($config_file);
-		$config->version = "0.6.1";
+		$config->version = "0.6.2";
 		$config->save();
 	}
 

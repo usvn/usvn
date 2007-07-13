2c012d79633598b01fee1656efcbaa3880d40654
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-07-13 09:47:14 +0000

    Display groups information
    refs #514

--- www/controllers/GroupController.php
+++ www/controllers/GroupController.php
@@ -20,4 +20,36 @@
 
 class GroupController extends USVN_Controller
 {
+	/**
+     * Pre-dispatch routines
+     *
+     * Called before action method. If using class with
+     * {@link Zend_Controller_Front}, it may modify the
+     * {@link $_request Request object} and reset its dispatched flag in order
+     * to skip processing the current action.
+     *
+     * @return void
+     */
+	public function preDispatch()
+	{
+		parent::preDispatch();
+
+		$group = $this->getRequest()->getParam('group');
+		$table = new USVN_Db_Table_Groups();
+		$group = $table->fetchRow(array("groups_name = ?" => $group));
+
+		$this->_group = $group;
+	}
+
+	/**
+	 * Group row object
+	 *
+	 * @var USVN_Db_Table_Row_Group
+	 */
+	protected $_group;
+
+	public function indexAction()
+	{
+		$this->view->group = $this->_group;
+	}
 }

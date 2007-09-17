3970dd1fc60a1fd7d47617be5d57de0ecde12fda
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-09-17 08:23:53 +0000

    Merged revisions 1077-1099,1101-1106 via svnmerge from https://svn.usvn.info/usvn/branches/0.6
    ........
      r1079 | abelaska | 2007-09-07 07:58:07 +0200 (ven, 07 sep 2007) | 3 lines
      
      Czech translation updated.
      
      refs #545
    ........
      r1082 | crivis_s | 2007-09-07 17:31:43 +0200 (ven, 07 sep 2007) | 6 lines
      
      
       * do not print warning if the first and only chmod() on the config file failed : if it fails, user creates it by himself and must check permission too
      
      refs #525
    ........
      r1090 | duponc_j | 2007-09-08 14:50:50 +0200 (sam, 08 sep 2007) | 2 lines
      
      Backport patch from stem
    ........
      r1104 | ecerutti | 2007-09-14 16:53:53 +0200 (ven, 14 sep 2007) | 3 lines
      
      Spanish translation (traduccion en español) of 0.6.4 ref #551
    ........
      r1105 | duponc_j | 2007-09-17 09:16:23 +0200 (lun, 17 sep 2007) | 5 lines
      
      Compilations of translations.
      
      
      closes #551, #550
    ........
      r1106 | duponc_j | 2007-09-17 10:18:07 +0200 (lun, 17 sep 2007) | 4 lines
      
      Apply patch from maki: Keep user modifications in authz file
      
      refs #563
    ........

--- www/USVN/AuthzTest.php
+++ www/USVN/AuthzTest.php
@@ -35,6 +35,7 @@ require_once 'www/USVN/autoload.php';
 class USVN_AuthzTest extends USVN_Test_DB {
 
 	private $_user;
+	private $_start = "# This is an auto generated file! Edit at your own risk!\n# You can edit this \"/\" section. Settings will be kept.\n#\n[/]\n* = \n\n#\n# Don't edit anything below! All manual changes will be overwritten. \n#\n\n[groups]\n";
 
 	public static function main() {
 		require_once "PHPUnit/TextUI/TestRunner.php";
@@ -45,10 +46,6 @@ class USVN_AuthzTest extends USVN_Test_DB {
 
 	protected function setUp() {
 		parent::setUp();
-/*		$configArray = array('subversion' => array('path' => 'tests/tmp/'));
-		$config = new Zend_Config($configArray);
-		Zend_Registry::set('config', $config);
-*/
 
 		$table = new USVN_Db_Table_Users();
 		$this->_user = $table->fetchNew();
@@ -66,7 +63,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		$table->delete(1);
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\n", $file);
+		$this->assertEquals($this->_start, $file);
 	}
 
 	public function testEmptyGroup()
@@ -78,7 +75,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		$group->save();
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ntoto = \n", $file);
+		$this->assertEquals($this->_start . "toto = \n", $file);
 	}
 
 	public function testOneUsersInOneGroup()
@@ -96,7 +93,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		$group->addUser($user1);
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ntoto = user1\n", $file);
+		$this->assertEquals($this->_start . "toto = user1\n", $file);
 	}
 
 	public function testTwoUsersInOneGroup()
@@ -116,7 +113,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		$group->addUser($user2);
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ntoto = user1, user2\n", $file);
+		$this->assertEquals($this->_start . "toto = user1, user2\n", $file);
 	}
 
 	public function testThreeUsersInOneGroup()
@@ -138,7 +135,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		$group->addUser($user3);
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ntoto = user1, user2, user3\n", $file);
+		$this->assertEquals($this->_start . "toto = user1, user2, user3\n", $file);
 	}
 
 	public function testOneUsersInTwoGroups()
@@ -154,7 +151,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		$group2->addUser($user1);
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ngroup1 = user1\ngroup2 = user1\n", $file);
+		$this->assertEquals($this->_start . "group1 = user1\ngroup2 = user1\n", $file);
 	}
 
 	public function testTwoUsersInTwoGroups()
@@ -171,7 +168,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		$group2->addUser($user2);
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ngroup1 = user1\ngroup2 = user2\n", $file);
+		$this->assertEquals($this->_start . "group1 = user1\ngroup2 = user2\n", $file);
 	}
 
 	public function testThreeUsersInTwoGroups()
@@ -192,7 +189,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		$group2->addUser($user2);
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ngroup1 = user1, user2, user3\ngroup2 = user1, user2\n", $file);
+		$this->assertEquals($this->_start . "group1 = user1, user2, user3\ngroup2 = user1, user2\n", $file);
 	}
 
 	public function testThreeUsersInTwoGroupsAndOneProject()
@@ -214,7 +211,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 
 		$project1 = USVN_Project::createProject(array('projects_name'  => "project1"), "test", true, false, false);
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ngroup1 = user1, user2, user3\ngroup2 = user1, user2\nproject1 = \n\n\n# Project project1\n[project1:/]\n@project1 = rw\n\n", $file);
+		$this->assertEquals($this->_start . "group1 = user1, user2, user3\ngroup2 = user1, user2\nproject1 = \n\n\n# Project project1\n[project1:/]\n@project1 = rw\n\n", $file);
 	}
 
 	public function testThreeUsersInTwoGroupsAndOneProjectWithPermission()
@@ -246,7 +243,7 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		}
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ngroup1 = user1, user2, user3\ngroup2 = user1, user2\nproject1 = \n\n\n# Project project1\n[project1:/]\n@project1 = rw\n\n[project1:/directory1]\n\n[project1:/directory2]\n\n[project1:/directory3]\n\n", $file);
+		$this->assertEquals($this->_start . "group1 = user1, user2, user3\ngroup2 = user1, user2\nproject1 = \n\n\n# Project project1\n[project1:/]\n@project1 = rw\n\n[project1:/directory1]\n\n[project1:/directory2]\n\n[project1:/directory3]\n\n", $file);
 	}
 
 	public function testThreeUsersInTwoGroupsAndOneProjectWithPermissionAndGroupPermissions()
@@ -316,7 +313,20 @@ class USVN_AuthzTest extends USVN_Test_DB {
 		}
 
 		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
-		$this->assertEquals("[/]\n* = \n\n[groups]\ngroup1 = user1, user2, user3\ngroup2 = user1, user2\ngroup3 = user1, user3\nproject1 = \nproject2 = \nproject3 = \n\n\n# Project project1\n[project1:/]\n@project1 = rw\n\n[project1:/directory1]\n@group1 = rw\n@group2 = \n@group3 = r\n\n[project1:/directory2]\n@group1 = rw\n@group2 = \n@group3 = \n\n[project1:/directory3]\n@group1 = rw\n@group2 = \n@group3 = r\n\n[project1:/directory4]\n@group1 = rw\n@group2 = \n@group3 = \n\n[project1:/directory5]\n@group1 = rw\n@group2 = \n@group3 = r\n\n\n\n# Project project2\n[project2:/]\n@project2 = rw\n\n[project2:/directory1]\n@group1 = r\n@group2 = rw\n@group3 = r\n\n[project2:/directory2]\n@group1 = \n@group2 = rw\n@group3 = \n\n[project2:/directory3]\n@group1 = r\n@group2 = rw\n@group3 = r\n\n[project2:/directory4]\n@group1 = \n@group2 = rw\n@group3 = \n\n[project2:/directory5]\n@group1 = r\n@group2 = rw\n@group3 = r\n\n\n\n# Project project3\n[project3:/]\n@project3 = rw\n\n[project3:/directory1]\n@group1 = r\n@group2 = \n@group3 = rw\n\n[project3:/directory2]\n@group1 = \n@group2 = \n@group3 = rw\n\n[project3:/directory3]\n@group1 = r\n@group2 = \n@group3 = rw\n\n[project3:/directory4]\n@group1 = \n@group2 = \n@group3 = rw\n\n[project3:/directory5]\n@group1 = r\n@group2 = \n@group3 = rw\n\n", $file);
+		$this->assertEquals($this->_start . "group1 = user1, user2, user3\ngroup2 = user1, user2\ngroup3 = user1, user3\nproject1 = \nproject2 = \nproject3 = \n\n\n# Project project1\n[project1:/]\n@project1 = rw\n\n[project1:/directory1]\n@group1 = rw\n@group2 = \n@group3 = r\n\n[project1:/directory2]\n@group1 = rw\n@group2 = \n@group3 = \n\n[project1:/directory3]\n@group1 = rw\n@group2 = \n@group3 = r\n\n[project1:/directory4]\n@group1 = rw\n@group2 = \n@group3 = \n\n[project1:/directory5]\n@group1 = rw\n@group2 = \n@group3 = r\n\n\n\n# Project project2\n[project2:/]\n@project2 = rw\n\n[project2:/directory1]\n@group1 = r\n@group2 = rw\n@group3 = r\n\n[project2:/directory2]\n@group1 = \n@group2 = rw\n@group3 = \n\n[project2:/directory3]\n@group1 = r\n@group2 = rw\n@group3 = r\n\n[project2:/directory4]\n@group1 = \n@group2 = rw\n@group3 = \n\n[project2:/directory5]\n@group1 = r\n@group2 = rw\n@group3 = r\n\n\n\n# Project project3\n[project3:/]\n@project3 = rw\n\n[project3:/directory1]\n@group1 = r\n@group2 = \n@group3 = rw\n\n[project3:/directory2]\n@group1 = \n@group2 = \n@group3 = rw\n\n[project3:/directory3]\n@group1 = r\n@group2 = \n@group3 = rw\n\n[project3:/directory4]\n@group1 = \n@group2 = \n@group3 = rw\n\n[project3:/directory5]\n@group1 = r\n@group2 = \n@group3 = rw\n\n", $file);
+	}
+
+	public function testKeepUserModificationsInAuthzFile()
+	{
+		$header = "# This is an auto generated file! Edit at your own risk!\n# You can edit this \"/\" section. Settings will be kept.\n#\n[/]\n* = r\n\n#\n# Don't edit anything below! All manual changes will be overwritten. \n#\n\n[groups]\n";
+		file_put_contents(Zend_Registry::get('config')->subversion->authz, $header);
+		$table = new USVN_Db_Table_Groups();
+		$table->delete(1);
+		$group = $table->fetchNew();
+		$group->name = "toto";
+		$group->save();
+		$file = file_get_contents(Zend_Registry::get('config')->subversion->authz);
+		$this->assertContains($header, $file);
 	}
 
 	/**

41c23864ec589c9bcd2d03d3e338dd34c69096bf
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-09-17 08:18:07 +0000

    Apply patch from maki: Keep user modifications in authz file
    refs #563

--- www/USVN/Authz.php
+++ www/USVN/Authz.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Class for autthz file manipulation
+ * Class for authz file manipulation
  *
  * @author Team USVN <contact@usvn.info>
  * @link http://www.usvn.info
@@ -11,7 +11,7 @@
  *
  * This software has been written at EPITECH <http://www.epitech.net>
  * EPITECH, European Institute of Technology, Paris - FRANCE -
- * This project has been realised as part of
+ * This project has been realized as part of
  * end of studies project.
  *
  * $Id$
@@ -21,7 +21,16 @@ class USVN_Authz
 	static public function generate()
 	{
 		$config = Zend_Registry::get('config');
-		$file = "[/]\n* = \n\n[groups]\n";
+
+		$oldUserConfig = self::loadExistingAuthzData($config->subversion->authz);
+		$text  = "# This is an auto generated file! Edit at your own risk!\n";
+		$text .= "# You can edit this \"/\" section. Settings will be kept.\n#\n";
+		$text .= "[/]\n";
+		foreach ($oldUserConfig as $key => $value) {
+			$text .= $key . " = " . $value . "\n";
+		}
+		$text .= "\n#\n# Don't edit anything below! All manual changes will be overwritten. \n#\n";
+		$text .= "\n[groups]\n";
 
 		$array = array();
 		$groups = new USVN_Db_Table_Groups();
@@ -30,8 +39,9 @@ class USVN_Authz
 		}
 		foreach ($array as $group => $users) {
 			$users = implode(", ", $users);
-			$file .= "{$group} = {$users}\n";
+			$text .= "{$group} = {$users}\n";
 		}
+
 
 		$array = array();
 		$projects = new USVN_Db_Table_Projects();
@@ -52,16 +62,73 @@ class USVN_Authz
 			}
 		}
 		foreach ($array as $project => $files_paths) {
-			$file .= "\n\n# Project {$project}\n";
+			$text .= "\n\n# Project {$project}\n";
 			foreach ($files_paths as $files_path => $files_rights) {
-				$file .= "[{$project}:{$files_path}]\n";
+				$text .= "[{$project}:{$files_path}]\n";
 				foreach ($files_rights as $group => $rights) {
-					$file .= "@{$group} = {$rights}\n";
+					$text .= "@{$group} = {$rights}\n";
 				}
-				$file .= "\n";
+				$text .= "\n";
 			}
 		}
 
-               @file_put_contents($config->subversion->authz, $file);
+        @file_put_contents($config->subversion->authz, $text);
+	}
+
+	/**
+	 * Loads the "/" Section from existing authz file.
+	 * All other lines and comments are ignored.
+	 *
+	 * @param String $fileName
+	 * @return array containing old lines
+	 */
+	static private function loadExistingAuthzData($fileName) {
+		$data = array("*" => "", ); // default values
+		try {
+			$rawdata = self::parse_ini_file($fileName);
+			if (array_key_exists("/", $rawdata)) {
+				$data = $rawdata['/'];
+			}
+		} catch (Exception $e) {
+			// we can ignore this, cause we preinitialized the data with an empty array.
+		}
+		return $data;
+	}
+
+	/**
+	 * Loads an INI file. Similar to built in parse_ini_file,
+	 * but accepts not only ";" but also "#" for comment lines.
+	 *
+	 * @param String $fileName
+	 * @return an array of all sections, each containing an array of key/value pairs.
+	 */
+	static private function parse_ini_file($fileName) {
+		$r = null;
+		$sec = null;
+		$f = @file($fileName);
+		if ($f === false) {
+			return array();
+		}
+		for ($i=0;$i<count($f);$i++)
+		{
+			$newsec=0;
+			$w=trim($f[$i]);
+			if ( ($w) and (substr($w,0,1)!="#") and (substr($w,0,1)!=";") )
+			{
+				if ((!$r) or ($sec))
+				{
+					if ((substr($w,0,1)=="[") and (substr($w,-1,1))=="]") {$sec=substr($w,1,strlen($w)-2);$newsec=1;}
+				}
+				if (!$newsec)
+				{
+					$w=explode("=",$w);
+					$k=trim($w[0]);
+					unset($w[0]);
+					$v=trim(implode("=",$w));
+					if ($sec) {$r[$sec][$k]=$v;} else {$r[$k]=$v;}
+				}
+			}
+		}
+		return $r;
 	}
 }

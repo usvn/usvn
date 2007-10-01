0fbb3fc45209fe573dd4eda0a8d79d3b24242989
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-10-01 13:34:01 +0000

    Indent
    refs #177

--- www/USVN/bootstrap.php
+++ www/USVN/bootstrap.php
@@ -23,75 +23,75 @@ require_once dirname(__FILE__) . '/autoload.php';
 date_default_timezone_set('UTC');
 error_reporting(E_ALL | E_STRICT);
 
-	/**
-	 * Load our ini conf file
-	 */
-	try {
-		$config = new USVN_Config_Ini(USVN_CONFIG_FILE, USVN_CONFIG_SECTION);
-		if (!isset($config->version)) {
-			header("Location: install");
-			exit(0);
-		}
-		if ($config->version != "0.6.4") {
-			header("Location: {$config->url->base}/update/{$config->version}/");
-			exit(0);
-		}
-	}
-	catch (Exception $e) {
+/**
+ * Load our ini conf file
+ */
+try {
+	$config = new USVN_Config_Ini(USVN_CONFIG_FILE, USVN_CONFIG_SECTION);
+	if (!isset($config->version)) {
 		header("Location: install");
 		exit(0);
 	}
-
-	date_default_timezone_set($config->timezone);
-
-	USVN_ConsoleUtils::setLocale($config->system->locale);
-
-	$routes_config = new USVN_Config_Ini(USVN_ROUTES_CONFIG_FILE, USVN_CONFIG_SECTION);
-
-	/**
-	 * Configure language
-	 */
-	USVN_Translation::initTranslation($config->translation->locale, USVN_LOCALE_DIRECTORY);
-
-	/**
-	 * Configure template
-	 */
-	USVN_Template::initTemplate($config->template->name, USVN_MEDIAS_DIRECTORY);
-
-	/**
-	 * register info
-	 */
-	Zend_Registry::set('config', $config);
-
-	/**
-	 * Configure our default db adapter
-	 */
-	Zend_Db_Table::setDefaultAdapter(Zend_Db::factory($config->database->adapterName, $config->database->options->toArray()));
-	if (isset($config->database->prefix)) {
-		USVN_Db_Table::$prefix = $config->database->prefix;
+	if ($config->version != "0.6.4") {
+		header("Location: {$config->url->base}/update/{$config->version}/");
+		exit(0);
 	}
+}
+catch (Exception $e) {
+	header("Location: install");
+	exit(0);
+}
+
+date_default_timezone_set($config->timezone);
+
+USVN_ConsoleUtils::setLocale($config->system->locale);
+
+$routes_config = new USVN_Config_Ini(USVN_ROUTES_CONFIG_FILE, USVN_CONFIG_SECTION);
+
+/**
+ * Configure language
+ */
+USVN_Translation::initTranslation($config->translation->locale, USVN_LOCALE_DIRECTORY);
+
+/**
+ * Configure template
+ */
+USVN_Template::initTemplate($config->template->name, USVN_MEDIAS_DIRECTORY);
+
+/**
+ * register info
+ */
+Zend_Registry::set('config', $config);
+
+/**
+ * Configure our default db adapter
+ */
+Zend_Db_Table::setDefaultAdapter(Zend_Db::factory($config->database->adapterName, $config->database->options->toArray()));
+if (isset($config->database->prefix)) {
+	USVN_Db_Table::$prefix = $config->database->prefix;
+}
 
 
-	/**
-	 * Get back the front controller and initialize some values
-	 */
-	$front = Zend_Controller_Front::getInstance();
-	$front->setRequest(new USVN_Controller_Request_Http());
+/**
+ * Get back the front controller and initialize some values
+ */
+$front = Zend_Controller_Front::getInstance();
+$front->setRequest(new USVN_Controller_Request_Http());
 
-	$front->throwExceptions(true);
+$front->throwExceptions(true);
 
-	$front->setBaseUrl($config->url->base);
+$front->setBaseUrl($config->url->base);
 
-	/**
-	 * Initialize router
-	 */
-	$router = new Zend_Controller_Router_Rewrite();
-	$router->addConfig($routes_config, 'routes');
+/**
+ * Initialize router
+ */
+$router = new Zend_Controller_Router_Rewrite();
+$router->addConfig($routes_config, 'routes');
 
-	$front->setRouter($router);
+$front->setRouter($router);
 
-	$front->setControllerDirectory(USVN_CONTROLLERS_DIR);
+$front->setControllerDirectory(USVN_CONTROLLERS_DIR);
 
-	$front->registerPlugin(new USVN_plugins_layout());
+$front->registerPlugin(new USVN_plugins_layout());
 
-	$front->dispatch();
+$front->dispatch();

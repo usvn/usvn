33dd174e1f87173d45b00beaf7b2c33e9c6e50ad
guyoll_o <guyoll_o@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2007-11-13 13:32:17 +0000

    #ref 601 and 615 Bugs in update file fixed and add the script for the flags not null

--- www/update/0.6.5/db.php
+++ www/update/0.6.5/db.php
@@ -23,7 +23,36 @@ function Sqlite_queries($db)
 {
 	$db->query("ALTER TABLE usvn_users_to_groups RENAME TO tmp");
 	$db->query("UPDATE tmp set is_leader = 0 WHERE is_leader IS NULL");
-	$db->query("CREATE TABLE usvn_users_to_groups AS SELECT * FROM tmp");
+	$db->query("CREATE TABLE usvn_users_to_groups(users_id int not null, groups_id int not null, is_leader bool not null, primary key (users_id, groups_id))");
+	$db->query("INSERT INTO usvn_users_to_groups select * from tmp");
+	$db->query("DROP TABLE tmp");
+	
+	
+	$db->query("ALTER TABLE usvn_groups_to_files_rights RENAME TO tmp");
+	$db->query("create table usvn_groups_to_files_rights
+	(
+	   files_rights_id int not null,  groups_id int not null, 
+	   files_rights_is_readable bool not null, files_rights_is_writable  bool not null,
+	   primary key (files_rights_id, groups_id)
+	)");
+	$db->query("INSERT INTO usvn_groups_to_files select * from tmp");
+	$db->query("DROP TABLE tmp");
+
+
+	$db->query("ALTER TABLE usvn_users RENAME TO tmp");
+	$db->query("
+	Create table usvn_users
+	(
+	   users_id int not null, users_login varchar(255) not null,
+	   users_password varchar(64) not null,
+	   users_lastname varchar(100),
+	   users_firstname varchar(100),
+	   users_email varchar(150),
+	   users_is_admin bool not null,
+	   CONSTRAINT USERS_LOGIN_UNQ UNIQUE (users_login),
+	   primary key (users_id)
+	)");
+	$db->query("INSERT INTO usvn_users select * from tmp");
 	$db->query("DROP TABLE tmp");
 	$db->closeConnection();
 }
@@ -33,6 +62,10 @@ function Mysql_queries ($db)
 {
 	$db->query("UPDATE usvn_users_to_groups set is_leader = 0 WHERE is_leader IS NULL");
 	$db->query("ALTER TABLE usvn_users_to_groups MODIFY (is_leader bool not null)");
+	$db->query("ALTER TABLE usvn_groups_to_files MODIFY (files_rights_is_readable bool not null)");
+	$db->query("ALTER TABLE usvn_groupd_to_files MODIFY (files_rightd_is_writable bool not null)");
+	$db->query("ALTER TABLE usvn_users MODIFY (users_is_admin bool not null)");
+	$db>closeConnection();
 }
 
 

835f1c5d5169c4db80a20017f97e39ccf358d3c0
duponc_j <duponc_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2008-01-20 16:04:26 +0000

    closes #618

--- www/USVN/bootstrap.php
+++ www/USVN/bootstrap.php
@@ -38,7 +38,7 @@ try {
 		header("Location: install");
 		exit(0);
 	}
-	if ($config->version != "0.7 RC5") {
+	if ($config->version != "0.7") {
 		header("Location: {$config->url->base}/update/{$config->version}/");
 		exit(0);
 	}

101a37c8c3a211d161afd3beabcd6ff2a4228f1f
meunie_j <meunie_j@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2009-09-19 22:29:39 +0000

    * Cleanup

--- create_archives.sh
+++ create_archives.sh
@@ -3,15 +3,15 @@
 old_pwd=`pwd`
 
 echo 'Exportation du projet dans /tmp/usvn ...'
-rm -rf /tmp/usvn
-svn export https://svn.usvn.info/usvn/trunk /tmp/usvn
-if [ ! -d /tmp/usvn ]; then
+rm -rf /tmp/usvn-1.0
+svn export https://svn.usvn.info/usvn/trunk /tmp/usvn-1.0
+if [ ! -d /tmp/usvn-1.0 ]; then
 	echo "Erreur d'exportation du projet !"
 	exit
 fi
 
 echo 'Suppression des fichiers inutiles ...'
-cd /tmp/usvn
+cd /tmp/usvn-1.0
 for i in `find . -name "*Test.php" | grep -v Zend`; do
 	rm -f "$i"
 done
@@ -19,8 +19,8 @@ rm -rf build.xml install* usvn.esproj create_archives.sh
 
 echo 'Creation des archives tar.gz et zip ...'
 version=`cat config/config.ini.exemple | grep -E "^version" | cut -d'"' -f2`
-cd "$old_pwd"
-tar cvzf "usvn-$version.tgz" /tmp/usvn
-zip -r "usvn-$version.zip" /tmp/usvn
+cd /tmp
+tar cvzf "$old_pwd/usvn-$version.tgz" ./usvn-1.0
+zip -r "$old_pwd/usvn-$version.zip" ./usvn-1.0
 
-rm -rf /tmp/usvn
+rm -rf /tmp/usvn-1.0

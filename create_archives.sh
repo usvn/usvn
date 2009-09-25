32fbf7db66b1de960d75bec8abcc20b8c1d691b9
monod-_p <monod-_p@b46fb6e3-c11b-0410-a9c8-e863d3774f8f>
2009-09-25 22:40:22 +0000

    - Petite modification de la creation d'archive, principalement pour avoir "usvn-1.0/..." au lieu de "./usvn-1.0/...", c'est moins perturbant quand on veut desarchiver  - mise a jour du numero de version dans le config.ini.exemple

--- create_archives.sh
+++ create_archives.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
+TMP_ROOT='/tmp'
+USVN='usvn-1.0'
+TMP_PATH="$TMP_ROOT/$USVN"
+CHECKOUT_URL='https://svn.usvn.info/usvn/trunk'
+
+die()
+{
+  echo
+  echo ">>   $*"
+  echo
+  exit 1
+}
 
 old_pwd=`pwd`
 
+# Exportation
 echo 'Exportation du projet dans /tmp/usvn ...'
-rm -rf /tmp/usvn-1.0
-svn export https://svn.usvn.info/usvn/trunk /tmp/usvn-1.0
-if [ ! -d /tmp/usvn-1.0 ]; then
-	echo "Erreur d'exportation du projet !"
-	exit
-fi
+rm -rf "$TMP_PATH" || die "Erreur de supression de $TMP_PATH"
+svn export "$CHECKOUT_URL" "$TMP_PATH" | sed 's/^/EXPORT: /' || die "Erreur d'exportation $CHECKOUT_URL dans $TMP_PATH"
 
+# Nettoyage
 echo 'Suppression des fichiers inutiles ...'
-cd /tmp/usvn-1.0
-for i in `find . -name "*Test.php" | grep -v Zend`; do
-	rm -f "$i"
-done
-rm -rf build.xml install* usvn.esproj create_archives.sh
+cd "$TMP_PATH"
+# Test files
+find . -name 'Zend' -prune -or -name "*Test.php" -exec rm -vf {} \; | sed 's/^/RM: /'
+# Misc Files
+rm -rvf build.xml install* usvn.esproj create_archives.sh | sed 's/^/RM: /'
 
+# Creation des Archives
 echo 'Creation des archives tar.gz et zip ...'
 version=`cat config/config.ini.exemple | grep -E "^version" | cut -d'"' -f2`
-cd /tmp
-tar cvzf "$old_pwd/usvn-$version.tgz" ./usvn-1.0
-zip -r "$old_pwd/usvn-$version.zip" ./usvn-1.0
+cd "$TMP_ROOT"
+tar cvzf "$old_pwd/usvn-$version.tgz" "$USVN" | sed 's/^/TAR: /'
+zip -r "$old_pwd/usvn-$version.zip" "$USVN" | sed 's/^/ZIP: /'
 
-rm -rf /tmp/usvn-1.0
+rm -rf "$TMP_PATH"

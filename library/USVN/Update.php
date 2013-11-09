a384baf20961cf2d1eb3107f27e215531ae76619
Julien Duponchelle <julien@duponchelle.info>
2013-11-09 11:35:59 +0100

    Release 1.0.7

--- library/USVN/Update.php
+++ library/USVN/Update.php
@@ -29,32 +29,37 @@ class USVN_Update
 		$config = new USVN_Config_Ini(USVN_CONFIG_FILE, USVN_CONFIG_SECTION);
 		if ($config->version == '1.0.0')
 		{
-			$config->version = '1.0.6';//DONT REPLACE WITH USVN_CONFIG_VERSION
+			$config->version = '1.0.7';//DONT REPLACE WITH USVN_CONFIG_VERSION
 			$config->save();
 		}
 		else if ($config->version == '1.0.1')//DONT REPLACE WITH USVN_CONFIG_VERSION
 		{
-			$config->version = '1.0.6';//DONT REPLACE WITH USVN_CONFIG_VERSION
+			$config->version = '1.0.7';//DONT REPLACE WITH USVN_CONFIG_VERSION
 			$config->save();
 		}
 		else if ($config->version == '1.0.2')//DONT REPLACE WITH USVN_CONFIG_VERSION
 		{
-			$config->version = '1.0.6';//DONT REPLACE WITH USVN_CONFIG_VERSION
+			$config->version = '1.0.7';//DONT REPLACE WITH USVN_CONFIG_VERSION
 			$config->save();
 		}
         else if ($config->version == '1.0.3')//DONT REPLACE WITH USVN_CONFIG_VERSION
 		{
-			$config->version = '1.0.6';//DONT REPLACE WITH USVN_CONFIG_VERSION
+			$config->version = '1.0.7';//DONT REPLACE WITH USVN_CONFIG_VERSION
 			$config->save();
 		}
         else if ($config->version == '1.0.4')//DONT REPLACE WITH USVN_CONFIG_VERSION
 		{
-			$config->version = '1.0.6';//DONT REPLACE WITH USVN_CONFIG_VERSION
+			$config->version = '1.0.7';//DONT REPLACE WITH USVN_CONFIG_VERSION
 			$config->save();
 		}
         else if ($config->version == '1.0.5')//DONT REPLACE WITH USVN_CONFIG_VERSION
         {
-            $config->version = '1.0.6';//DONT REPLACE WITH USVN_CONFIG_VERSION
+            $config->version = '1.0.7';//DONT REPLACE WITH USVN_CONFIG_VERSION
+            $config->save();
+        }
+        else if ($config->version == '1.0.6')//DONT REPLACE WITH USVN_CONFIG_VERSION
+        {
+            $config->version = '1.0.7';//DONT REPLACE WITH USVN_CONFIG_VERSION
             $config->save();
         }
         else

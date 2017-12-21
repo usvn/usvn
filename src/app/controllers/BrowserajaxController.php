19b78f128ecc6683f2bcc19ef001745703e8fcfc
Cedric Boudinet <boudinpg@gmail.com>
2017-12-21 11:04:54 +0100

    replacing split (deprecated in php 5.3.0, removed in php 7.0.0) by explode

--- src/app/controllers/BrowserajaxController.php
+++ src/app/controllers/BrowserajaxController.php
@@ -215,8 +215,8 @@ class BrowserajaxController extends USVN_Controller
 
 			$msg = "Ok";
 
-			$tabgroup = split(",", $_GET['group']);
-			$tabrights = split(",", $_GET['rights']);
+			$tabgroup = explode(",", $_GET['group']);
+			$tabrights = explode(",", $_GET['rights']);
 			$j = 0;
 			foreach ($tabgroup as $group)
 			{
